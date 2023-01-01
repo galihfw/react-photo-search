61f089fb8370b8f07f2363d73d59fe7ded843e4c
DESKTOP-NGISFV2\galih <galihfw23@gmail.com>
2023-01-02 02:20:47 +0700

    up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1 className="title">React Photo Search</h1>
         <SearchPhotos />
       </div>
     </div>
